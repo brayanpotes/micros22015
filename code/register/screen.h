ab9e1ea58739c6ef782affcd864f791fc8bcb62a
ucsystem <ucsystem@github.com>
2015-08-18 22:48:46 -0500

    Código ejemplo para mostrar el valor de los registros de un procesador

---
+++ code/register/screen.h
@@ -0,0 +1,12 @@
+#ifndef _SCREEN_H_
+#define	_SCREEN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "colors.h"
+
+void showRegisters(uint32_t *registers, size_t len);
+
+#endif	/* _SCREEN_H_ */
